cf505f2131346c0a19acec6bb42d135e50cb5586
Fazna Harees <faznaaharees@gmail.com>
2024-11-28 23:52:30 +0530

    changed site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,20 @@ import React from "react";
 
 export default function Main() {
   const { query } = useRouter();
+  const flowers_code = `import turtle
+
+t = turtle.Turtle()
+petals = 6
+angle = 360/petals
+
+for i in range(petals):  #
+    t.circle(50)
+    t.right(angle) 
+
+turtle.done()`;
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(flowers_code);
+  };
   return (
     <div className="w-screen min-h-screen p-20 flex items-center">
       <div className="w-full rounded-2xl min-h-[50vh] h-full p-10 flex flex-col gap-3 items-center justify-center bg-gradient-to-b from-gray-700 to-gray-800">
@@ -26,6 +40,12 @@ export default function Main() {
         >
           Turtle Coding
         </a>
+        <button
+          className="bg-white text-black p-4 rounded-md"
+          onClick={copyToClipboard}
+        >
+          Copy Flowers Code
+        </button>
         {query?.q === "admin" && (
           <a
             href="https://trinket.io/turtle"
